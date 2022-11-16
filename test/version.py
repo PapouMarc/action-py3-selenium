9635a2cd93fb4aca2826e7b11e865c095338763f
PapouMarc <50478268+PapouMarc@users.noreply.github.com>
2022-11-16 14:01:06 +0100

    ajout version module

--- test/version.py
+++ test/version.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
 
 import sys
+import pkg_resources
+
+installed_packages = pkg_resources.working_set
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
 print(sys.version)
+print(installed_packages_list)
