d05c938c0243f7b0660480b4e6a88dd020e7390a
PapouMarc <50478268+PapouMarc@users.noreply.github.com>
2022-11-16 14:09:13 +0100

    Update version.py
    ajout multiligne

--- test/version.py
+++ test/version.py
@@ -5,5 +5,8 @@ import pkg_resources
 
 installed_packages = pkg_resources.working_set
 installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
-print(sys.version)
-print(installed_packages_list)
+_text=sys.version
+for _n in installed_packages_list:
+  _text = '{}\n{}'.format(_text,_n)
+
+ print(_text)
